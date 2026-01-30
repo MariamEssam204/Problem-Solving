c1af63b98c3cfd773caa1e28fc4a2d1f5692d179
Mariam Essam <116377304+MariamEssam204@users.noreply.github.com>
2026-01-30 13:22:13 +0200

    Create A. Maximum GCD.cpp

---
+++ Codeforces/A. Maximum GCD.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/1370/A
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <set>
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;cin>>t;
+    while(t--){
+        ll n;cin>>n;
+        ll maxi =n/2;
+        cout<<maxi<<"\n";
+    }
+    return 0;
+}
